d859aa453f3898f0456024f91fa5bc711c546c07
Kamil <kamil.muzyka91@gmail.com>
2020-12-23 19:33:48 +0100

    corrected styles

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import SignInPage from "../SignIn";
-import akacjoweLogo from "../../Assets/akacjoweLogo.png";
 
 const Landing = () => (
   <section className="landingPage">
-    <img className="logo" src={akacjoweLogo} alt="logo" />
     <SignInPage />
   </section>
 );
