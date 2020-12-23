b69566b60c58c18068e8eb01368d6460999d8976
Kamil <kamil.muzyka91@gmail.com>
2020-12-23 16:14:54 +0100

    add Account component

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
- 
-const Account = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+import React from "react";
+
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
+import {
+  AuthUserContext,
+  withAuthorization,
+  withEmailVerification,
+} from "../Session";
+import { compose } from "recompose";
+import Navigation from "../Navigation";
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {(authUser) => (
+      <div>
+        <Navigation />
+        <h1>
+          Zalogowany: {authUser.username} {authUser.email}
+        </h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
- 
-export default Account;
+
+// to samo co w homePage
+const condition = (authUser) => !!authUser;
+
+export default compose(
+  withEmailVerification,
+  withAuthorization(condition)
+)(AccountPage);
