38176b2c360044e4cdfd4551f512084dce6c29f3
Kamil <kamil.muzyka91@gmail.com>
2020-12-23 16:28:11 +0100

    add sign in comp

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,98 @@
-import React from 'react';
- 
-const SignIn = () => (
-  <div>
-    <h1>App</h1>
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
+
+import { SignUpLink } from "../SignUp";
+import { PasswordForgetLink } from "../PasswordForget";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
+
+const SignInPage = () => (
+  <div className="signIn__form">
+    <SignInForm />
+    <div className="signIn__option">
+      <PasswordForgetLink />
+      <SignUpLink />
+    </div>
   </div>
 );
- 
-export default SignIn;
+
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+  error: null,
+};
+
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const { email, password } = this.state;
+
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = password === "" || email === "";
+
+    return (
+      <form className="signIn__form__input" onSubmit={this.onSubmit}>
+        <p className="signIn__form__title">Akacjowe przedszkole</p>
+        <p className="signIn__input__name"> E-mail:</p>
+        <input
+          className="signIn__form__input__label"
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="akacjowe@przedszkole.pl"
+        />
+        <br />
+        <p className="signIn__input__name"> Hasło:</p>
+        <input
+          className="signIn__form__input__label"
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Hasło"
+        />
+        <br />
+        <button
+          className="signIn__send__btn"
+          disabled={isInvalid}
+          type="submit"
+        >
+          Zaloguj
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
+
+export default SignInPage;
+
+export { SignInForm };
