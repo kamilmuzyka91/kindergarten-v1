8d4a5f1c4d4d8ab7507278a8469687eb1d700d01
Kamil <kamil.muzyka91@gmail.com>
2020-12-23 16:19:27 +0100

    add Navigation comp

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
- 
-const Navigation = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+import React from "react";
+import { Link } from "react-router-dom";
+
+import * as ROUTES from "../../constants/routes";
+import * as ROLES from "../../constants/roles";
+import SignOutButton from "../SignOut";
+import { AuthUserContext } from "../Session";
+
+const Navigation = ({ authUser }) => (
+  <AuthUserContext.Consumer>
+    {(authUser) =>
+      authUser ? (
+        <NavigationAuth authUser={authUser} />
+      ) : (
+        // ready to NavigationNonAuth
+        <NavigationAuth authUser={authUser} />
+      )
+    }
+  </AuthUserContext.Consumer>
 );
- 
+
+const NavigationAuth = ({ authUser }) => (
+  <ul>
+    <li>
+      <Link to={ROUTES.HOME}>Strona główna</Link>
+    </li>
+    {!!authUser.roles[ROLES.ADMIN] && (
+      <li>
+        <Link to={ROUTES.ADMIN}>Panel administracyjny</Link>
+      </li>
+    )}
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Profil</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.CHAT}>Czat</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
 export default Navigation;
