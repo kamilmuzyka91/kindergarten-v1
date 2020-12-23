f621367193b854959c44265894d7a5d5808fbe63
Kamil <kamil.muzyka91@gmail.com>
2020-12-23 16:17:56 +0100

    add Home comp

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
- 
-const Home = () => (
+import React from "react";
+
+import { withAuthorization } from "../Session";
+import Navigation from "../Navigation";
+
+const HomePage = () => (
   <div>
-    <h1>App</h1>
+    <Navigation />
+    <h1>Home Page</h1>
+    <br />
+    <p>Home Page jest dostępna dla wszystkich zarejestrowanych użytkowników</p>
+    <br />
+    <br />
+    <section className="list">
+      <article className="kids_games">img gry</article>
+      <article className="kids_order">img zamówienia</article>
+      <article className="kids_complaints">img skargi</article>
+      <article className="kids_plans">img plany</article>
+    </section>
   </div>
 );
- 
-export default Home;
+//  warunek autoryzacji, tylko zalogowany użytkownik może zobaczyć homePage (nie tylko autoryzowany admin - każdy user)
+const condition = (authUser) => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
